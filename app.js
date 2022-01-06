0f3c8e6b24865de5ca256d9269db3f4ba6481adb
darwhite08 <chandrapriyanshu10@gmail.com>
2022-01-07 03:51:15 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+const ejs = require("ejs");
+
+const app = express()
+const port = process.env.PORT
+
+app.use("/public", express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+app.get('/', function(req, res) {
+    res.render('services');
+});
+app.get('/home', function(req, res) {
+    res.redirect("/");
+})
+app.get('/about', function(req, res) {
+    res.render('about');
+});
+app.get('/services', function(req, res) {
+    res.render('services');
+});
+app.get('/portfolio', function(req, res) {
+    res.render("portfolio");
+});
+app.get('/contact', function(req, res) {
+    res.render("contact");
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
