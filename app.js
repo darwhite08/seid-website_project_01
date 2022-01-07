424f778139613bd2f97a9a2eb4727cc596594179
darwhite08 <chandrapriyanshu10@gmail.com>
2022-01-07 22:21:27 +0530

    fourth commit

--- app.js
+++ app.js
@@ -3,27 +3,16 @@ const bodyParser = require("body-parser")
 const ejs = require("ejs");
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.use("/public", express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
-app.get('/', function(req, res) {
-    res.render('main');
-});
-app.get('/home', function(req, res) {
-    res.redirect("/");
-})
-app.get('/about', function(req, res) {
-    res.render('about');
-});
-app.get('/services', function(req, res) {
-    res.render('services');
-});
-app.get('/portfolio', function(req, res) {
-    res.render("portfolio");
-});
-app.get('/contact', function(req, res) {
-    res.render("contact");
-});
+app.get('/', function(req, res) { res.render("contact"); });
+// app.get('/home', function(req, res) { res.redirect("/"); });
+// app.get('/about', function(req, res) { res.render('about'); });
+// app.get('/services', function(req, res) { res.render('services'); });
+// app.get('/portfolio', function(req, res) { res.render("portfolio"); });
+// app.get('/contact', function(req, res) { res.render("contact"); });
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
