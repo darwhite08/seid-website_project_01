959fd127258d7a0558b517ddd3e71d58e3394c37
darwhite08 <chandrapriyanshu10@gmail.com>
2022-01-07 22:40:20 +0530

    commit five

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const port = process.env.PORT || 3000
 app.use("/public", express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
-app.get('/', function(req, res) { res.render("contact"); });
-// app.get('/home', function(req, res) { res.redirect("/"); });
-// app.get('/about', function(req, res) { res.render('about'); });
-// app.get('/services', function(req, res) { res.render('services'); });
-// app.get('/portfolio', function(req, res) { res.render("portfolio"); });
-// app.get('/contact', function(req, res) { res.render("contact"); });
+app.get('/', function(req, res) { res.render("main"); });
+app.get('/home', function(req, res) { res.redirect("/"); });
+app.get('/about', function(req, res) { res.render('about'); });
+app.get('/services', function(req, res) { res.render('services'); });
+app.get('/portfolio', function(req, res) { res.render("portfolio"); });
+app.get('/contact', function(req, res) { res.render("contact"); });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
