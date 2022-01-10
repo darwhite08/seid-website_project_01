91e97bc645d73218b436e88af9076a2ce8e0c3f9
darwhite08 <chandrapriyanshu10@gmail.com>
2022-01-11 01:10:29 +0530

    8 commit

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser")
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use("/public", express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }))
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true })
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User = new mongoose.model("User", userSchema)
 
 app.get('/', function(req, res) { res.render("main"); });
 app.get('/home', function(req, res) { res.redirect("/"); });
 app.get('/about', function(req, res) { res.render('about'); });
-app.get('/services', function(req, res) { res.render('services'); });
+app.get('/services', function(req, res) { res.render("services"); });
 app.get('/portfolio', function(req, res) { res.render("portfolio"); });
 app.get('/contact', function(req, res) { res.render("contact"); });
+app.get('/admin', function(req, res) { res.sendFile(__dirname + "\\login.html") });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
